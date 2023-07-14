0523d1bcc33c66e6b07822fbf9761d1455834875
Magotgarang <m.guut@alustudent.com>
2023-07-14 12:51:25 +0000

    updated

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,11 +1,11 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """DOCS"""
 import requests
 
 
 def top_ten(subreddit):
-    """DOCS"""
-    reddit_url = https://www.reddit.com/r/{}/hot.json" \
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
         .format(subreddit)
     headers = headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
@@ -14,5 +14,5 @@ def top_ten(subreddit):
         data = response.json()['data']
         for post in data['children'][:10]:
             print(post['data']['title'])
-else:
-    print(None)
+    else:
+        print(None)
