245fe5b5d414389a204186a4597915b4fe091792
Magotgarang <m.guut@alustudent.com>
2023-07-14 13:19:29 +0000

    updated

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -22,5 +22,4 @@ def recurse(subreddit, hot_list=[], after=None):
         [hot_list.append(article.get('data').get('title'))
          for article in hot_articles]
 
-        return recurse(subreddit, hot_list, after=after) \
-            if has_next else hot_list
+        return recurse(subreddit, hot_list, after=after) \            if has_next else hot_list
